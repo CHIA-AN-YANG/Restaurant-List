924ea09402d36e6d6aec320958f832165ab148f0
Chia-An Yang <chiaan.y.creativeworker@gmail.com>
2021-06-03 14:40:26 +0800

    :pencil: (env.example) write local vs remote env.example files

--- models/seeds/userShopSeeder.js
+++ models/seeds/userShopSeeder.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const bcrypt = require('bcryptjs')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -5,24 +6,49 @@ if (process.env.NODE_ENV !== 'production') {
 const Shop = require('../shop')
 const User = require('../user')
 const db = require('../../config/mongoose')
-const shopDocs = require('./shopSeeder')
 
-const SEED_USER = {
-  name: 'foodie',
-  email: 'foodie@seeder.com',
-  password: 'foodlover'
-}
+//Divide shop data with required length for each user
+//未來考慮寫更多user的seeder file，儘量降低耦合少使用常數
+let shopRawData = require('./shopSeeder')
+shopDocs = _.chunk(shopRawData, 3)
+
+const SEED_USER = [
+  {
+    name: 'seedUser1',
+    email: 'user1@example.com',
+    password: '12345678'
+  },
+  {
+    name: 'seedUser2',
+    email: 'user2@example.com',
+    password: '12345678'
+  }
+]
+
 db.once('open', () => {
-  bcrypt
+  bcrypt //nested structure: 未來可改寫為函式再引用進來
     .genSalt(10)
-    .then(salt => bcrypt.hash(SEED_USER.password, salt))
-    .then(hash => User.create({user_name: SEED_USER.name, user_email: SEED_USER.email, password: hash}))
+    .then(salt => bcrypt.hash(SEED_USER[0].password, salt))
+    .then(hash => User.create({user_name: SEED_USER[0].name, user_email: SEED_USER[0].email, password: hash}))
     .then(user => { 
-      Array.from({ length: shopDocs.length }, (_, i)=>{shopDocs[i].userId = user._id})
-      return Shop.insertMany(shopDocs)
-    })
-    .then(() => {
-      console.log(`finish seeding with 1 user and ${shopDocs.length} shop doc`)
-      process.exit()
-    })
+      const arr = shopDocs[0]
+      Array.from({ length: arr.length }, (_, i)=>{arr[i].userId = user._id})
+      Shop.insertMany(arr)
+      console.log('first user ok!')
+    }).then(() => {
+      bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(SEED_USER[1].password, salt))
+        .then(hash => User.create({user_name: SEED_USER[1].name, user_email: SEED_USER[1].email, password: hash}))
+        .then(user => { 
+          const arr = shopDocs[1]
+          Array.from({ length: arr.length }, (_, i)=>{arr[i].userId = user._id})
+          return Shop.insertMany(arr)
+        })
+        .catch(error => console.log(error))
+      })
+      .then(() => {
+        console.log(`finish seeding with 2nd user and 3 shop docs`)      
+      })
+      .catch(error => console.log(error))
 })
